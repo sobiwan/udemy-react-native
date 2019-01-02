ed674b61c3e951cb73c934261fc3c95b69c9f066
sobiwan <smobrien914@gmail.com>
2019-01-02 17:44:34 -0600

    FB authentication project complete

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Loading, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 
 
 class App extends Component { 
+
+    state = { auth: null }
 
     componentWillMount() {
         firebase.initializeApp({
@@ -17,15 +19,49 @@ class App extends Component {
             storageBucket: "auth-384b4.appspot.com",
             messagingSenderId: "1034747444918"
           });
+        
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ auth: true });
+            } else {
+                this.setState({ auth: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.auth){
+            case true:
+             return (
+                 <CardSection>
+                    <Button onPress={() => firebase.auth().signOut()}> 
+                     Log Out 
+                    </Button>
+                </CardSection>)
+            case false:
+             return <LoginForm />
+            default:
+             return (
+                 <View style={styles.loading}>
+                    <Loading size='large' />
+                 </View>
+             )
+        }        
     }
 
     render () {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
+    }
+}
+
+const styles = {
+    loading: {
+        paddingTop: '10%'
     }
 }
 
