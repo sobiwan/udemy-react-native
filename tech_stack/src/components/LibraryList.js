094a63904775190ba721a44e77e38dbc27216061
sobiwan <smobrien914@gmail.com>
2019-01-03 13:19:58 -0600

    test prettier

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -14,6 +14,7 @@ class LibraryList extends Component {
         data={this.props.libraries}
         renderItem={this.renderItem}
         keyExtractor={library => `${library.id}`}
+        
       />
     );
   }
