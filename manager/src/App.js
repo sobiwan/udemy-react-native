3dc0ed48d1e77695e2c3004514ca6103756bce29
sobiwan <smobrien914@gmail.com>
2019-01-03 14:14:12 -0600

    init employee manager app

---
+++ manager/src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+class App extends Component { 
+
+  render () {
+      return (
+        <Provider store={createStore(reducers)}>
+          <View>
+            <Text>Hello World</Text>
+          </View>
+        </Provider>
+      );
+  }
+}
+
+
+export default App; 
